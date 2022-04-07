b0b1b8fbd369c32ddfb3849bd5297318788961e8
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-07 21:49:27 +0200

    Setup echo server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/quantumsheep/leboncoin-fizzbuzz/handlers"
+	"github.com/quantumsheep/leboncoin-fizzbuzz/handlers/handlers_validator"
+)
+
+func main() {
+	e := echo.New()
+	e.Validator = handlers_validator.NewCustomValidator()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
